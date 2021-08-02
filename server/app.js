ae8aa7831f1a11eead4518e95225da75b8266f02
Krist Veruari <kveruari@gmail.com>
2021-08-02 11:03:20 -0600

    added functions

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 //const { request, response } = require('express');
 dotenv.config();
 
-const dbServer = require('./dbservice');
+const dbService = require('./dbService');
 
 //We will be able to send data to our back end
 app.use(cors());
@@ -17,19 +17,58 @@ app.use(express.urlencoded({ extended : false}));
 //Create our routes and the basic of mysql queries
 //create 
 app.post('/insert', (request, response) => {
+    console.log(request.body);
 
 });
 //read
 app.get('/getAll', (request, response) => {
     //console.log('test');
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+    /*
     response.json({
         success: true
     });
+    */
 });
 //update
+app.patch('/update', (request, response) => {
+    const { id, name } = request.body;
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.updateNameById(id, name);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
 //delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.deleteRowById(id);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/search/:name', (request, response) => {
+    const { name } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.searchByName(name);
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+});
 
 //Make sure our local server is working
 app.listen(process.env.PORT, () => console.log('app is running'));
